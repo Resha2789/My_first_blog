f0d21fae4ab6361b853d9ef19605386fab0425cd
resha2789 <resha2789@gmail.com>
2021-05-01 07:20:22 +0500

    До обновления сущности Article

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -44,7 +44,7 @@ class Comment
 
     /**
      * @ORM\ManyToOne(targetEntity=User::class, inversedBy="comments")
-     * @ORM\JoinColumn(name="user_id", nullable=true)
+     * @ORM\JoinColumn(name="user_id", nullable=false)
      */
     private $userName;
 
